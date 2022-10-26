fda29c1422af628c3524c7ee589b96ad3ea7634c
Maksim Botalov <dev.botalov@gmail.com>
2022-10-26 18:22:58 +0500

    Initial Commit

---
+++ Balancer/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Balancer
+//
+//  Created by Максим Боталов on 26.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Start"
+    }
+}
+
